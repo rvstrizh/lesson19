bff251c6ab5bd75c37750349d02c1efb5154394e
The Art of Development <theartdevel@gmail.com>
2021-11-14 21:05:05 +0300

    fix

--- demostration/part2/intro_ep1_ep2_ep3_ep4/service/user.py
+++ demostration/part2/intro_ep1_ep2_ep3_ep4/service/user.py
@@ -20,24 +20,24 @@ class UserService:
         return self.dao.get_by_username(username)
 
     def create(self, user_d):
-        user_d["password"] = self.make_user_password_hash(user_d.get("password"))
+#        user_d["password"] = self.make_user_password_hash(user_d.get("password"))
         return self.dao.create(user_d)
 
     def update(self, user_d):
-        user_d["password"] = self.make_user_password_hash(user_d.get("password"))
+#        user_d["password"] = self.make_user_password_hash(user_d.get("password"))
         self.dao.update(user_d)
         return self.dao
 
     def delete(self, rid):
         self.dao.delete(rid)
 
-    def make_user_password_hash(self, password):
-        return base64.b64encode(hashlib.pbkdf2_hmac(
-            'sha256',
-            password.encode('utf-8'),
-            PWD_HASH_SALT,
-            PWD_HASH_ITERATIONS
-        ))
+    # def make_user_password_hash(self, password):
+    #     return base64.b64encode(hashlib.pbkdf2_hmac(
+    #         'sha256',
+    #         password.encode('utf-8'),
+    #         PWD_HASH_SALT,
+    #         PWD_HASH_ITERATIONS
+    #     ))
 
     def compare_passwords(self, password_hash, other_password) -> bool:
         return hmac.compare_digest(
